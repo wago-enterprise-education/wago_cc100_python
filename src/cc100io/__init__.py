ccfdf35cdb0eef194353e9ae19629e8bb198212b
Andre Alexander Bell <andre.bell@wago.com>
2025-02-26 11:04:04 +0100

    Renamed _core to io

--- src/cc100io/__init__.py
+++ src/cc100io/__init__.py
@@ -2,6 +2,6 @@
 
 from importlib.metadata import version
 
-from ._core import digitalWrite, analogWrite, digitalRead, digitalReadWait, analogRead, delay, tempRead
+from .io import digitalWrite, analogWrite, digitalRead, digitalReadWait, analogRead, delay, tempRead
 
 __version__ = version("cc100io")
