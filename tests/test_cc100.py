1040f679ccf808be94e9f662194d3b2e76ac5724
Johann Timm <johann.timm@wago.com>
2026-02-16 15:32:47 +0100

    add assertEqual functions

--- tests/test_cc100.py
+++ tests/test_cc100.py
@@ -10,18 +10,34 @@ class Test_cc100(unittest.TestCase):
         self.assertEqual(cm.output, ["WARNING:CC100IO:Output does not exist"])
 
     def test_digital_write(self):
-        for i in range(1, 9):
+        for i in range(1, 5):
             for j in range(2):
                 cc.digitalWrite(i,j)
-            
+
+    def test_non_existing_input(self):
+        with self.assertLogs(level="WARNING") as cm:
+                cc.digitalRead(9)
+        self.assertEqual(cm.output, ["WARNING:CC100IO:Input does not exist"])
+              
     def test_digital_read(self):
         for i in range(1,9):
             cc.digitalRead(i)
             
+    def test_non_existing_analog_output(self):
+        with self.assertLogs(level="WARNING") as cm:
+            cc.analogWrite(3)
+        self.assertEqual(cm.output,["WARNING:CC100IO:Output does not exist"])
+
+
     def test_analog_write(self):
         for i in range(1,2):
             for j in range(0,10001,1000):
                 cc.analogWrite(i,j)
+        
+    def test_non_existing_analog_input(self):
+        with self.assertLogs(level="WARNING") as cm:
+            cc.analogRead(3)
+        self.assertEqual(cm.output,["WARNING:CC100IO:Input does not exist"])
 
     def test_analog_read(self):
         for i in range(1,2):
@@ -66,3 +82,5 @@ class Test_cc100(unittest.TestCase):
     def TestIsDocker(self):
         pass
 
+if __name__ == '__main__':
+    unittest.main()
